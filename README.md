d598fc7cef6d2c5bb875e0cca16d3116fd188e88
l_saintgermain_catie <l.saintgermain@catie.fr>
2024-08-09 15:58:27 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bosch Sensortec GmbH BNO055 Sensor Driver
+
+Bosch Sensortec GmbH BNO055 9-axis absolute orientation sensor driver for Zephyr OS.
---
+++ drivers/sensor/bosch/bno055/bno055.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2024, CATIE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT bosch_bno055
+
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+
+#include "bno055.h"
+
+LOG_MODULE_REGISTER(BNO055, CONFIG_SENSOR_LOG_LEVEL);
+
+struct bno055_config {
+};
+
+struct bno055_data {
+};
+
+static int bno055_attr_set(const struct device *dev, enum sensor_channel chan,
+			   enum sensor_attribute attr, const struct sensor_value *val)
+{
+	return 0;
+}
+
+static int bno055_sample_fetch(const struct device *dev, enum sensor_channel chan)
+{
+	struct bno055_data *data = dev->data;
+	const struct bno055_config *config = dev->config;
+
+	return 0;
+}
+
+static int bno055_channel_get(const struct device *dev, enum sensor_channel chan,
+			      struct sensor_value *val)
+{
+	struct bno055_data *data = dev->data;
+
+	// TODO: Update val with the sensor value
+	val->val1 = 0;
+	val->val2 = 0;
+
+	return 0;
+}
+
+static int bno055_init(const struct device *dev)
+{
+	const struct bno055_config *config = dev->config;
+	struct bno055_data *data = dev->data;
+
+	return 0;
+}
+
+static const struct sensor_driver_api bno055_driver_api = {
+	.attr_set = bno055_attr_set,
+	.sample_fetch = bno055_sample_fetch,
+	.channel_get = bno055_channel_get,
+};
+
+#define BNO055_INIT(n)                                                                             \
+	static struct bno055_config bno055_config_##n = {                                             \
+	};                                                                                         \
+	static struct bno055_data bno055_data_##n;                                                 \
+	DEVICE_DT_INST_DEFINE(n, bno055_init, NULL, &bno055_data_##n, &bno055_config_##n,          \
+			      POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY, &bno055_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(BNO055_INIT)
