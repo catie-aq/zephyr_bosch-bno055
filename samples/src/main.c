fc695ad34c044fdf4e96ce9c58f5d066f2865e40
l_saintgermain_catie <l.saintgermain@catie.fr>
2024-09-17 10:18:10 +0200

    Fixed issue with callback array

--- samples/src/main.c
+++ samples/src/main.c
@@ -9,14 +9,16 @@
 #include <bno055.h> // Required for custom SENSOR_CHAN_*
 
 static const struct device *const bno_dev = DEVICE_DT_GET(DT_NODELABEL(bno0550));
+static struct sensor_trigger trig_acc_drdy;
 static struct sensor_trigger trig_acc_asn;
 static struct sensor_trigger trig_acc_hg;
 static struct sensor_trigger trig_gyr_am;
 static struct sensor_trigger trig_gyr_hr;
 
 static bool trigger_an_motion = false;
+static bool bno055_fusion = false;
 
-void acc_data_ready(const struct device *dev, const struct sensor_trigger *trigger)
+void acc_drdy(const struct device *dev, const struct sensor_trigger *trigger)
 {
 	printk("BSX data ready!!\n");
 }
@@ -49,8 +51,6 @@ void gyr_hr(const struct device *dev, const struct sensor_trigger *trigger)
 
 int main(void)
 {
-	struct sensor_value acc[3], lia[3], grav[3], eul[3], quat[4], calib[4];
-
 #if DEVICE_DT_DEFER(DT_NODELABEL(bno0550))
 	k_sleep(K_MSEC(BNO055_TIMING_STARTUP));
 	device_init(bno_dev);
@@ -62,13 +62,17 @@ int main(void)
 	}
 
 	struct sensor_value config = {
-		.val1 = BNO055_MODE_ACC_MAG_GYRO,
+		.val1 = (bno055_fusion) ? BNO055_MODE_NDOF : BNO055_MODE_ACC_MAG_GYRO,
 		.val2 = 0,
 	};
 	sensor_attr_set(bno_dev, SENSOR_CHAN_ALL, SENSOR_ATTR_CONFIGURATION, &config);
 	config.val1 = BNO055_POWER_NORMAL;
 	config.val2 = 0;
 	sensor_attr_set(bno_dev, SENSOR_CHAN_ALL, BNO055_SENSOR_ATTR_POWER_MODE, &config);
+
+	trig_acc_drdy.type = SENSOR_TRIG_DATA_READY;
+	trig_acc_drdy.chan = SENSOR_CHAN_ACCEL_XYZ;
+	sensor_trigger_set(bno_dev, &trig_acc_drdy, acc_drdy);
 
 	if (trigger_an_motion) {
 		config.val1 = BNO055_ACC_DURATION_AM;
@@ -139,33 +143,40 @@ int main(void)
 	sensor_attr_set(bno_dev, SENSOR_CHAN_GYRO_XYZ, SENSOR_ATTR_FEATURE_MASK, &config);
 
 	while (1) {
-		// sensor_sample_fetch(bno_dev);
+		sensor_sample_fetch(bno_dev);
 
 		// Example for Linear Acceleration and Gravity
-		sensor_channel_get(bno_dev, SENSOR_CHAN_ACCEL_XYZ, acc);
-		sensor_channel_get(bno_dev, BNO055_SENSOR_CHAN_LINEAR_ACCEL_XYZ, lia);
-		sensor_channel_get(bno_dev, BNO055_SENSOR_CHAN_GRAVITY_XYZ, grav);
-		sensor_channel_get(bno_dev, BNO055_SENSOR_CHAN_EULER_YRP, eul);
-		sensor_channel_get(bno_dev, BNO055_SENSOR_CHAN_QUATERNION_WXYZ, quat);
-		sensor_channel_get(bno_dev, BNO055_SENSOR_CHAN_CALIBRATION_SGAM, calib);
+		if (!bno055_fusion) {
+			struct sensor_value acc[3];
+			sensor_channel_get(bno_dev, SENSOR_CHAN_ACCEL_XYZ, acc);
 
-		printk("ACCEL: X(m.s-2)[%d.%06d] Y(m.s-2)[%d.%06d] Z(m.s-2)[%d.%06d]\n",
-		       acc[0].val1, acc[0].val2, acc[1].val1, acc[1].val2, acc[2].val1,
-		       acc[2].val2);
-		// printk("LINACCEL: X(m.s-2)[%d.%06d] Y(m.s-2)[%d.%06d] Z(m.s-2)[%d.%06d]\n",
-		//        lia[0].val1, lia[0].val2, lia[1].val1, lia[1].val2, lia[2].val1,
-		//        lia[2].val2);
-		// printk("GRAVITY: X(m.s-2)[%d.%06d] Y(m.s-2)[%d.%06d] Z(m.s-2)[%d.%06d]\n",
-		//        grav[0].val1, grav[0].val2, grav[1].val1, grav[1].val2, grav[2].val1,
-		//        grav[2].val2);
-		// printk("EULER: X(rad.s-1)[%d.%06d] Y(rad.s-1)[%d.%06d] Z(rad.s-1)[%d.%06d]\n",
-		//        eul[0].val1, eul[0].val2, eul[1].val1, eul[1].val2, eul[2].val1,
-		//        eul[2].val2);
-		// printk("QUATERNION: W[%d.%06d] X[%d.%06d] Y[%d.%06d] Z[%d.%06d]\n", quat[0].val1,
-		//        quat[0].val2, quat[1].val1, quat[1].val2, quat[2].val1, quat[2].val2,
-		//        quat[2].val1, quat[2].val2);
-		// printk("CALIB: SYS[%d] GYR[%d] ACC[%d] MAG[%d]\n", calib[0].val1, calib[1].val1,
-		//        calib[2].val1, calib[3].val1);
+			printk("ACCEL: X(m.s-2)[%d.%06d] Y(m.s-2)[%d.%06d] Z(m.s-2)[%d.%06d]\n",
+			       acc[0].val1, acc[0].val2, acc[1].val1, acc[1].val2, acc[2].val1,
+			       acc[2].val2);
+		} else {
+			struct sensor_value lia[3], grav[3], eul[3], quat[4], calib[4];
+			sensor_channel_get(bno_dev, BNO055_SENSOR_CHAN_LINEAR_ACCEL_XYZ, lia);
+			sensor_channel_get(bno_dev, BNO055_SENSOR_CHAN_GRAVITY_XYZ, grav);
+			sensor_channel_get(bno_dev, BNO055_SENSOR_CHAN_EULER_YRP, eul);
+			sensor_channel_get(bno_dev, BNO055_SENSOR_CHAN_QUATERNION_WXYZ, quat);
+			sensor_channel_get(bno_dev, BNO055_SENSOR_CHAN_CALIBRATION_SGAM, calib);
+
+			printk("LINACCEL: X(m.s-2)[%d.%06d] Y(m.s-2)[%d.%06d] Z(m.s-2)[%d.%06d]\n",
+			       lia[0].val1, lia[0].val2, lia[1].val1, lia[1].val2, lia[2].val1,
+			       lia[2].val2);
+			printk("GRAVITY: X(m.s-2)[%d.%06d] Y(m.s-2)[%d.%06d] Z(m.s-2)[%d.%06d]\n",
+			       grav[0].val1, grav[0].val2, grav[1].val1, grav[1].val2, grav[2].val1,
+			       grav[2].val2);
+			printk("EULER: X(rad.s-1)[%d.%06d] Y(rad.s-1)[%d.%06d] "
+			       "Z(rad.s-1)[%d.%06d]\n",
+			       eul[0].val1, eul[0].val2, eul[1].val1, eul[1].val2, eul[2].val1,
+			       eul[2].val2);
+			printk("QUATERNION: W[%d.%06d] X[%d.%06d] Y[%d.%06d] Z[%d.%06d]\n",
+			       quat[0].val1, quat[0].val2, quat[1].val1, quat[1].val2, quat[2].val1,
+			       quat[2].val2, quat[2].val1, quat[2].val2);
+			printk("CALIB: SYS[%d] GYR[%d] ACC[%d] MAG[%d]\n", calib[0].val1,
+			       calib[1].val1, calib[2].val1, calib[3].val1);
+		}
 
 		k_sleep(K_MSEC(500));
 	}
